ffbd3719439fad98182d9639f7b512d07343b0d3
Tom Elliott <tom@ocuroot.com>
2025-11-13 15:05:52 -0500

    Test parallel workers
    Start up multiple workers at once and verify that they don't do unnecessary work.

--- tests/worker/test.sh
+++ tests/worker/test.sh
@@ -61,6 +61,72 @@ test_worker_push() {
     echo ""
 }
 
+
+test_multi_worker_push() {
+     # Clean up test data
+    rm -rf ./testdata
+
+    # Call the init function to create repositories and set environment variables
+    export REPO_REMOTE=$(init_repo "./testdata/remote")
+
+    # Set up working directory with empty initial commit
+    init_working_dir "./testdata/source" "$REPO_REMOTE" "SOURCE_WORKING"
+
+    cp ./src/repo1/commit1/* "./testdata/source"
+
+    pushd "./testdata/source" >> /dev/null
+
+    # Start worker in the source directory, with dev mode
+    echo "Starting 3 workers, will log to: ../worker{1,2,3}.log"
+    ocuroot start worker --dev --interval 1s > ../worker1.log 2>&1 &
+    assert_equal "0" "$?" "Failed to start worker 1"
+    ocuroot start worker --dev --interval 1s > ../worker2.log 2>&1 &
+    assert_equal "0" "$?" "Failed to start worker 2"
+    ocuroot start worker --dev --interval 1s > ../worker3.log 2>&1 &
+    assert_equal "0" "$?" "Failed to start worker 3"
+    sleep 3 # Allow the workers to come up
+
+    # Apply first commit
+    git add .
+    git commit -m "Add source"
+
+    # Wait until deploys are complete
+    wait_for_ref "push/-/a.ocu.star/@/deploy/production"
+    wait_for_ref "push/-/b.ocu.star/@/deploy/production"
+
+    assert_deployed "a.ocu.star" "production"
+    assert_deployed "b.ocu.star" "production"
+    assert_ref_equals "push/-/a.ocu.star/@/deploy/production#output/message" "Message at commit 1"
+    assert_ref_equals "push/-/b.ocu.star/@/deploy/production#output/message" "Message at commit 1"
+
+    # Apply second commit
+    cp ../../src/repo1/commit2/* "./"
+
+    git add .
+    git commit -m "Update message"
+
+    # Wait until deploys are complete
+    wait_for_ref "push/-/a.ocu.star/@r2/deploy/production"
+
+    assert_deployed "a.ocu.star" "production"
+    assert_deployed "b.ocu.star" "production"
+    assert_ref_equals "push/-/a.ocu.star/@/deploy/production#output/message" "Message at commit 2"
+    assert_ref_equals "push/-/b.ocu.star/@/deploy/production#output/message" "Message at commit 2"
+
+    # Ensure b did not get another full release
+    check_ref_does_not_exist "push/-/b.ocu.star/@r2/deploy/production"
+    # Ensure the parallel workers did not cause extra work
+    check_ref_does_not_exist "push/-/b.ocu.star/@r1/deploy/production/3"
+
+    # Show all refs as a check
+    ocuroot state match "**"
+
+    popd >> /dev/null
+
+    echo "Test succeeded"
+    echo ""
+}
+
 test_worker_intent() {
     # Clean up test data
     rm -rf ./testdata
@@ -113,6 +179,7 @@ build_ocuroot
 pushd "$(dirname "$0")" > /dev/null
 
 test_worker_push
+test_multi_worker_push
 test_worker_intent
 
 popd
